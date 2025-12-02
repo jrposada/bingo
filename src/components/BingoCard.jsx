a0f27ddf38976469f8b1f2f373f2f03eccfbd7b1
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2025-12-02 11:39:13 +0100

    feat: use css grid

--- src/components/BingoCard.jsx
+++ src/components/BingoCard.jsx
@@ -1,73 +1,55 @@
-import React, { useRef } from 'react';
-import './BingoCard.css';
+import React, { useRef } from "react";
+import "./BingoCard.css";
 
-const BingoCard = ({ 
-  cardData, 
-  backgroundImage, 
-  showBorders = true, 
-  paddingTop = 0, 
-  paddingRight = 0, 
-  paddingBottom = 0, 
+const BingoCard = ({
+  cardData,
+  backgroundImage,
+  showBorders = true,
+  paddingTop = 0,
+  paddingRight = 0,
+  paddingBottom = 0,
   paddingLeft = 0,
   imageScale = 100,
   rowGap = 0,
-  columnGap = 0
+  columnGap = 0,
 }) => {
   const cardRef = useRef(null);
 
   return (
-    <div 
-      ref={cardRef}
-      className="bingo-card-wrapper"
-      id="bingo-card"
-    >
-      <div 
-        className={`bingo-card ${!showBorders ? 'no-borders' : ''}`}
+    <div ref={cardRef} className="bingo-card-wrapper" id="bingo-card">
+      <div
+        className={`bingo-card ${!showBorders ? "no-borders" : ""}`}
         style={{
-          backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none'
+          backgroundImage: backgroundImage ? `url(${backgroundImage})` : "none",
         }}
       >
-        <div 
+        <div
           className="bingo-grid"
           style={{
             paddingTop: `${paddingTop}px`,
             paddingRight: `${paddingRight}px`,
             paddingBottom: `${paddingBottom}px`,
             paddingLeft: `${paddingLeft}px`,
-            height: '100%',
-            width: '100%',
-            rowGap: `${rowGap}px`,
-            columnGap: `${columnGap}px`
+            height: `calc(100% - (${paddingBottom}px))`,
+            width: `calc(100% - (${paddingRight}px))`,
           }}
         >
-          {cardData.map((row, rowIndex) => (
-            <div 
-              key={rowIndex} 
-              className="bingo-row"
-              style={{
-                gap: `${columnGap}px`
-              }}
-            >
-              {row.map((cell, colIndex) => (
-                <div 
-                  key={`${rowIndex}-${colIndex}`} 
-                  className="bingo-cell"
-                  style={{
-                    background: 'transparent',
-                    padding: `${(100 - imageScale)}%`
-                  }}
-                >
-                  {cell && (
-                    <img 
-                      src={cell} 
-                      alt={`Cell ${rowIndex}-${colIndex}`}
-                      className="cell-image"
-                    />
-                  )}
-                </div>
-              ))}
-            </div>
-          ))}
+          {cardData.map((row, rowIndex) =>
+            row.map((cell, colIndex) => (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                className="bingo-cell"
+              >
+                {cell && (
+                  <img
+                    src={cell}
+                    alt={`Cell ${rowIndex}-${colIndex}`}
+                    className="cell-image"
+                  />
+                )}
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
